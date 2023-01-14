0ac71bf075b2e8b609adbd425c6ceac9ba43ae8e
Patrick Beagan <pbeagan1@gmail.com>
2023-01-14 10:34:56 -0600

    successfully using closures

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,32 @@ use std::net::IpAddr;
 use local_ip_address::local_ip;
 
 fn main() {
-    let pretty_ip = prettify_ip(get_local_ip());
-    println!("{}", pretty_ip);
+    let adjs: [&str; 256] = adjective_words::get_words();
+    let re = Regex::new(r"\d+").unwrap();
 
-    let pretty_ip = prettify_ip_colors(get_local_ip());
+    let pretty_ip: String = prettify_ip(get_local_ip(), &|text: &str| {
+        let is_match = re.is_match(text);
+        return if is_match {
+            let index = text.parse::<usize>().ok().unwrap();
+            let res: &str = adjs[index];
+            res.to_string()
+        } else {
+            text.to_string()
+        };
+    });
+    
+    println!("{}", pretty_ip);
+    let colors: [&str; 16] = adjective_words::get_colors();
+
+    let pretty_ip = prettify_ip(get_local_ip(), &|text| {
+        let is_match = re.is_match(text);
+        return if is_match {
+            let index = text.parse::<usize>().ok().unwrap();
+             format!("{}{}", colors[index / 16], colors[index % 16])
+        } else {
+            text.to_string()
+        };
+    });
     println!("{}", pretty_ip);
 
     //    let stdin = io::stdin();
@@ -17,53 +39,18 @@ fn main() {
     //        process_line(&adjs, &line.unwrap());
     //    }
 }
-fn prettify_ip(ip: IpAddr) -> String {
-    let adjs: [&str; 256] = adjective_words::get_words();
-    let re = Regex::new(r"\d+").unwrap();
-
-    let output: String = ip.to_string();
-    let output: &str = output.as_str();
-    let output: Vec<&str> = output.split(".").collect();
-    let output: Vec<&str> = output
-        .iter()
-        .map(|text| {
-            let is_match = re.is_match(text);
-            return if is_match {
-                let index = text.parse::<usize>().ok().unwrap();
-                let res: &str = adjs[index];
-                res
-            } else {
-                text
-            };
-        })
-        .collect::<Vec<&str>>();
-
-    return output.join(".");
-}
-
-fn prettify_ip_colors(ip: IpAddr) -> String {
-    let colors: [&str; 16] = adjective_words::get_colors();
-    let re = Regex::new(r"\d+").unwrap();
-
+fn prettify_ip(ip: IpAddr, on_each: &dyn Fn(&str) -> String) -> String {
     let output: String = ip.to_string();
     let output: &str = output.as_str();
     let output: Vec<&str> = output.split(".").collect();
     let output: Vec<String> = output
         .iter()
-        .map(|text| {
-            let is_match = re.is_match(text);
-            return if is_match {
-                let index = text.parse::<usize>().ok().unwrap();
-                let r: String = format!("{}{}", colors[index / 16], colors[index % 16]);
-                r
-            } else {
-                text.to_string()
-            };
-        })
+        .map(|each| on_each(each))
         .collect::<Vec<String>>();
 
     return output.join(".");
 }
+
 fn get_local_ip() -> IpAddr {
     let my_local_ip = local_ip();
     if let Ok(my_local_ip) = my_local_ip {
