9b532eb1a00cd5f149a97f61518a2a095f70a4b0
Patrick Beagan <pbeagan1@gmail.com>
2023-01-16 00:35:56 -0600

    Updating with animals in the dictionary

--- src/util.rs
+++ src/util.rs
@@ -3,14 +3,26 @@ use std::net::IpAddr;
 use local_ip_address::local_ip;
 
 pub fn prettify_ip(ip: IpAddr, on_each: &dyn Fn(&str) -> String) -> Vec<String> {
-    let output: String = ip.to_string();
-    let output: &str = output.as_str();
-    let output: Vec<&str> = output.split(".").collect();
-    let output: Vec<String> = output
+    return ip
+        .to_string()
+        .as_str()
+        .split(".")
+        .collect::<Vec<&str>>()
         .iter()
         .map(|each| on_each(each))
         .collect::<Vec<String>>();
-    return output;
+}
+
+pub fn prettify_ip_indexed(ip: IpAddr, on_each: &dyn Fn(usize, &str) -> String) -> Vec<String> {
+    return ip
+        .to_string()
+        .as_str()
+        .split(".")
+        .collect::<Vec<&str>>()
+        .iter()
+        .enumerate()
+        .map(|each| on_each(each.0, each.1))
+        .collect::<Vec<String>>();
 }
 
 pub fn get_local_ip() -> IpAddr {
